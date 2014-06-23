79817d0f6652157d4a5e7bf02a23a06f1744dc18
Will <zhangyanrui@sina.cn>
2014-06-23 17:42:10 +0800

    Initial Commit

---
+++ ChineseToPinYinDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ChineseToPinYinDemo
+//
+//  Created by Will on 14-6-23.
+//  Copyright (c) 2014年 Will. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ChineseToPinYinDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ChineseToPinYinDemo
+//
+//  Created by Will on 14-6-23.
+//  Copyright (c) 2014年 Will. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
